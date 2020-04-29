a6014e5c174c25734243d051ae5e533cc9404cda
dannegm <dannegm@gmail.com>
2020-04-29 02:56:02 -0500

    Favicon

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ const App = () => (
         <h1>Hello!</h1>
         <h2>I'm Daniel García.</h2>
         <p>I'm working on this site, sorry.</p>
-        <p>Now, I'm testing my CI/CD...</p>
-        <p>... Again...</p>
+        <p>Now, I'm testing my CI/CD... :D</p>
     </div>
 )
 
