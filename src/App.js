e36327085519d57ede769ca9e8c947e4d0e1b6f7
dannegm <dannegm@gmail.com>
2020-04-29 02:39:47 -0500

    Update basic description

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 const App = () => (
     <div className="App">
         <h1>Hello!</h1>
+        <h2>I'm Daniel García.</h2>
+        <p>I'm working on this site, sorry.</p>
     </div>
 )
 
