398be53c42f1564f47a10f529c5b60cee67922b9
dannegm <im@danielgarcia.me>
2022-04-02 23:41:34 -0600

    -

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -17,18 +17,12 @@ import {
     NftBadge,
     DescritionContainer,
     Hello,
-    Name,
-    Title,
-    Description,
-    Link,
-    SocialLinks,
-    SocialLink,
 } from './Home.styled';
 
 const $avatar = getTodayAvatar();
 
 const Home = () => {
-    useDocumentTitle('Hello!, I’m Daniel García');
+    useDocumentTitle('Hello!');
 
     const todayBackground = $avatar?.bgComponent;
 
@@ -54,43 +48,6 @@ const Home = () => {
                     </AvatarContainer>
                     <DescritionContainer>
                         <Hello>Hello!</Hello>
-                        <Name>
-                            I’m <b>Daniel García</b>,
-                        </Name>
-                        <Title>Software Specialist.</Title>
-                        <Description>
-                            Currently may find me at{' '}
-                            <Link href='https://www.wizeline.com/'>
-                                Wizeline
-                            </Link>{' '}
-                            coding something awesome.
-                        </Description>
-                        <SocialLinks>
-                            <SocialLink
-                                color='#d1d5da'
-                                href='https://github.com/dannegm'
-                            >
-                                Github
-                            </SocialLink>
-                            <SocialLink
-                                color='#1da1f2'
-                                href='https://twitter.com/dannegm'
-                            >
-                                Twitter
-                            </SocialLink>
-                            <SocialLink
-                                color='#0a66c2'
-                                href='https://www.linkedin.com/in/dannegm'
-                            >
-                                LinkedIn
-                            </SocialLink>
-                            <SocialLink
-                                color='#dd2a7b'
-                                href='https://www.instagram.com/dannegm'
-                            >
-                                Instagram
-                            </SocialLink>
-                        </SocialLinks>
                     </DescritionContainer>
                 </Hero>
             </Overlay>
