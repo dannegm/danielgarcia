60180ac8f2b18a0699cbbdbc36cea4cb44deff84
dannegm <im@danielgarcia.me>
2025-04-21 20:05:06 -0600

    dynamic avatar

--- src/app/avatare/route.js
+++ src/app/avatare/route.js
@@ -1,8 +1,88 @@
 import { join } from 'path';
 import { readFile } from 'fs/promises';
+import {
+    isBdaySeason,
+    isHackersDay,
+    isIndependenceDay,
+    isJulySix,
+    isKidsDay,
+    isMay4th,
+    isNight,
+    isNov5th,
+    isPanBday,
+    isRevolutionDay,
+    isSpookySeason,
+    isValentinDay,
+    isXmasSeason,
+} from './rules';
+
+const commonAvatar = '/img/avatares/avatare.png';
+
+const getConditionalAvatar = (defaultAvatar, maps) => {
+    const reverseMaps = [...maps]; //.reverse();
+    return reverseMaps.reduce((q, { condition, mapper }) => {
+        if (!condition) return q;
+        return mapper?.(q);
+    }, defaultAvatar);
+};
+
+const conditionalAvatar = getConditionalAvatar(commonAvatar, [
+    {
+        condition: isNight(),
+        mapper: () => '/img/avatares/avatare-noir.png',
+    },
+    {
+        condition: isValentinDay(),
+        mapper: () => '/img/avatares/avatare-valentin.png',
+    },
+    {
+        condition: isBdaySeason(),
+        mapper: () => '/img/avatares/avatare-daftpunk.png',
+    },
+    {
+        condition: isKidsDay(),
+        mapper: () => '/img/avatares/avatare-kid.png',
+    },
+    {
+        condition: isMay4th(),
+        mapper: () => '/img/avatares/avatare-vader.png',
+    },
+    {
+        condition: isPanBday(),
+        mapper: () => '/img/avatares/avatare-interestepan.png',
+    },
+    {
+        condition: isJulySix(),
+        mapper: () => '/img/avatares/avatare-julysix.png',
+    },
+    {
+        condition: isHackersDay(),
+        mapper: () => '/img/avatares/avatare-hackerman.png',
+    },
+    {
+        condition: isIndependenceDay(),
+        mapper: () => '/img/avatares/avatare-mex.png',
+    },
+    {
+        condition: isSpookySeason(),
+        mapper: () => '/img/avatares/avatare-pumpkin.png',
+    },
+    {
+        condition: isNov5th(),
+        mapper: () => '/img/avatares/avatare-guy-fawkes.png',
+    },
+    {
+        condition: isRevolutionDay(),
+        mapper: () => '/img/avatares/avatare-mex.png',
+    },
+    {
+        condition: isXmasSeason(),
+        mapper: () => '/img/avatares/avatare-xmas.png',
+    },
+]);
 
 export const GET = async () => {
-    const filePath = join(process.cwd(), 'public', '/img/avatares/avatare.png');
+    const filePath = join(process.cwd(), 'public', conditionalAvatar);
     const imageBuffer = await readFile(filePath);
 
     return new Response(imageBuffer, {
---
+++ src/app/avatare/rules.js
@@ -0,0 +1,92 @@
+import {
+    isWithinInterval,
+    setHours,
+    setMinutes,
+    setSeconds,
+    addDays,
+    isSameDay,
+    getDayOfYear,
+} from 'date-fns';
+
+export const isNight = (now = new Date()) => {
+    const startTime = setSeconds(setMinutes(setHours(new Date(), 20), 0), 0); // 8:00 PM
+    const endTime = setSeconds(setMinutes(setHours(new Date(), 6), 0), 0); // 2:00 AM (del día siguiente)
+
+    const adjustedEndTime = endTime < startTime ? addDays(endTime, 1) : endTime; // Ajusta si cruza la medianoche
+
+    return isWithinInterval(now, { start: startTime, end: adjustedEndTime });
+};
+
+export const isValentinDay = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 1, 14);
+    return isSameDay(today, date);
+};
+
+export const isBdaySeason = (today = new Date()) => {
+    const currentMonth = today.getMonth();
+    const currentDay = today.getDate();
+
+    if (currentMonth === 3 && currentDay > 25) return true;
+    if (currentMonth === 3 && currentDay < 20) return true;
+
+    return false;
+};
+
+export const isKidsDay = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 3, 30);
+    return isSameDay(today, date);
+};
+
+export const isMay4th = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 4, 4);
+    return isSameDay(today, date);
+};
+
+export const isPanBday = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 4, 17);
+    return isSameDay(today, date);
+};
+
+export const isJulySix = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 6, 6);
+    return isSameDay(today, date);
+};
+
+export const isHackersDay = (today = new Date()) => {
+    return getDayOfYear(today) === 256;
+};
+
+export const isIndependenceDay = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 8, 16);
+    return isSameDay(today, date);
+};
+
+export const isSpookySeason = (today = new Date()) => {
+    const currentMonth = today.getMonth();
+    const currentDay = today.getDate();
+
+    if (currentMonth === 9) return true;
+    if (currentMonth === 10 && currentDay < 3) return true;
+
+    return false;
+};
+
+export const isNov5th = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 10, 5);
+    return isSameDay(today, date);
+};
+
+export const isRevolutionDay = (today = new Date()) => {
+    const date = new Date(today.getFullYear(), 10, 20);
+    return isSameDay(today, date);
+};
+
+export const isXmasSeason = (today = new Date()) => {
+    const currentMonth = today.getMonth();
+    const currentDay = today.getDate();
+
+    if (currentMonth === 11) return true;
+    if (currentMonth === 0 && currentDay < 10) return true;
+
+    return false;
+};
