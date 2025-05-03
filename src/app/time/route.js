d31ae224f7139c03a2b6d0520e9fcc7d7768067f
dannegm <im@danielgarcia.me>
2025-05-03 14:58:15 -0600

    Update timezone initialization and include limited flag in time API response

--- src/app/time/route.js
+++ src/app/time/route.js
@@ -4,15 +4,19 @@ export async function GET(request) {
 
     const ip = request.headers.get('x-forwarded-for') || '8.8.8.8';
     let client = 'unknown';
-    let timezone = 'unknown';
+    let timezone = 'America/Mexico_City';
+    let limited = true;
 
     try {
         const geoRes = await fetch(`https://ipapi.co/${ip}/timezone`);
         timezone = await geoRes.text();
+        limited = false;
+    } catch (e) {
+    } finally {
         if (timezone.includes('/')) {
             client = new Date().toLocaleString('en-US', { timeZone });
         }
-    } catch {}
+    }
 
-    return Response.json({ server, client, timezone });
+    return Response.json({ server, client, timezone, limited });
 }
